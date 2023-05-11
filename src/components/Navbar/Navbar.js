61b5b8813f83a03e704efa6164ccfad6f03b1377
mdArifulIslam12 <mdarif767778@gmail.com>
2023-05-11 11:51:08 +0600

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+    
+    const [backgroundColor, setBackgroundColor] = useState(false);
+  
+    const allScroolId = () => {
+      if (window.scrollY >= 8) {
+        setBackgroundColor(true);
+      } else {
+        setBackgroundColor(false);
+      }
+    };
+    window.addEventListener("scroll", allScroolId);
+  
+    return (
+      <div className="fixed-top">
+        <nav
+          className={
+            backgroundColor
+              ? `navbar navbar-expand-lg navbar-light  bg-white shadow-lg`
+              : `navbar navbar-expand-lg navbar-light  navbar-background`
+          }
+          id="scroll"
+        >
+          <div className="container py-2 ">
+            <img
+              className="header-nav-img"
+              src="https://travo.iamabdus.com/v1.0/wp-content/uploads/2021/04/Group-2.svg"
+              alt=""
+            />
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav ms-auto mb-2 mt-2 mb-lg-0">
+                <li className="nav-item">
+                  <NavLink to={"/"} className="navbar-links">
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to={"/tours"} className="navbar-links">
+                    Tours
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to={"/destination"} className="navbar-links">
+                    Destination
+                  </NavLink>
+                </li>
+                {/* <li className="nav-item">
+                  {user && (
+                    <NavLink to={"/dashborad"} className="navbar-links">
+                      Dashborad
+                    </NavLink>
+                  )}
+                </li> */}
+                <li className="nav-item">
+                  <NavLink to={"/blogs"} className="navbar-links">
+                    Blogs
+                  </NavLink>
+                </li>
+  
+                <li className="nav-item">
+                  <NavLink to={"/about"} className="navbar-links">
+                    About Us
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to={"/contactUs"} className="navbar-links">
+                    Contact Us
+                  </NavLink>
+                </li>
+  
+                {/* <li className="nav-item">
+                  {user ? (
+                    <a href="" onClick={() => signOut(auth)}>
+                      Sign out
+                    </a>
+                  ) : (
+                    <CustomLink to={"/loginV2"} className="navbar-links">
+                      Login
+                    </CustomLink>
+                  )}
+                </li> */}
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  };
+export default Navbar;
